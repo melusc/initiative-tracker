979eecd87e429a787afd71830c83a79fef364839
Luca Schnellmann <oss@lusc.ch>
2025-02-14 10:48:51 +1100

    Add space between `(C)` and author

--- workspaces/util/src/slug.test.ts
+++ workspaces/util/src/slug.test.ts
@@ -1,5 +1,5 @@
 /*!
-Copyright (C)Luca Schnellmann, 2025
+Copyright (C) Luca Schnellmann, 2025
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
